9267e0becb66261f8d28892247ff1eef831833d9
mwienhoven <mwienhoven2@gmail.com>
2025-11-06 18:37:31 +0100

    Fixed the image path and added some comments to the serve.py.

--- model/serve.py
+++ model/serve.py
@@ -5,31 +5,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from loguru import logger
 
+
+# Load the processed data
 datadir = Path("data/processed")
 if not datadir.exists():
     logger.info(f"Creating directory {datadir}")
     datadir.mkdir(parents=True)
+
+# Read the data
 datafile = datadir / Path("posts.parquet")
 df = pd.read_parquet(datafile)
-df.head()
+logger.info(f"Data shape: {df.shape}")
 
+# Initialize the clustering model
 clustering = TextClustering()
 
-
-k = 100
+# Perform clustering
+k = 100  # Number of clusters
 X = clustering(df["text"], k=k, batch=True, method="PCA")
-X.shape
+logger.info(f"Clustering completed with {k} clusters. Resulting shape: {X.shape}")
 
+# Get cluster labels
 labels = clustering.get_labels(df)
 
-
+# Visualize the clusters
 plt.figure(figsize=(10, 10))
 sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=labels)
 
-imgdir = Path("../img")
+# Save the plot
+imgdir = Path("./img")
 if not imgdir.exists():
     print(f"Creating directory {imgdir}")
     imgdir.mkdir(parents=True)
 
 imgfile = imgdir / Path("clustering.png")
 plt.savefig(imgfile)
+logger.info(f"Clustering plot saved to {imgfile}")
